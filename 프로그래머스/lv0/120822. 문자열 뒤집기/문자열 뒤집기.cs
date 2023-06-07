27966bbaf9dd7568229a74e7ab50a764447252b4
JCURVEs <97331501+JCURVEs@users.noreply.github.com>
2023-06-07 19:28:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.55 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.cs
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.cs
@@ -2,9 +2,8 @@ using System;
 
 public class Solution {
     public string solution(string my_string) {
-        char[] charArray = my_string.ToCharArray();
+        var charArray = my_string.ToCharArray();
         Array.Reverse(charArray);
-        string answer = string.Join("", charArray);
-        return answer;
+        return new string(charArray);
     }
 }
