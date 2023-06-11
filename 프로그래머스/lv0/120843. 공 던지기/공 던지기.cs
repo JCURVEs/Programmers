ab459104a2be520680d5cabda27fa9cd70076867
JCURVEs <97331501+JCURVEs@users.noreply.github.com>
2023-06-11 15:25:02 +0900

    [level 0] Title: 공 던지기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int ball= 1;
+        for(int i = 0; i < k - 1; i++)
+        {
+            if(ball + 2 > numbers.Length)
+            {
+                ball += 2;
+                ball -= numbers.Length;
+            }
+            else
+            {
+                ball += 2;
+            }
+        }
+        answer = ball;
+        return answer;
+    }
+}
