4b96426ce9885bc863f598c3583897632d659536
JCURVEs <97331501+JCURVEs@users.noreply.github.com>
2023-06-16 13:58:23 +0900

    [level 0] Title: 약수 구하기, Time: 0.52 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.cs
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.cs
@@ -1,23 +1,17 @@
 using System;
+using System.Collections.Generic;
 
-public class Solution {
-    public int[] solution(int n) {
-        int sqrt = (int)Math.Sqrt(n);
-        int[] divisors = new int[sqrt * 2];
-        int count = 0;
-        
-        for (int i = 1; i <= sqrt; i++) {
-            if (n % i == 0) {
-                divisors[count++] = i;
-                
-                if (n / i != i) {
-                    divisors[count++] = n / i;
-                }
-            }
+public class Solution
+{
+    public int[] solution(int n)
+    {
+        List<int> answer = new List<int>();
+        for(int i=1; i <= n; i++)
+        {
+            if(n % i == 0)
+            
+                answer.Add(i);
         }
-        
-        Array.Resize(ref divisors, count);
-        Array.Sort(divisors);
-        return divisors;
+            return answer.ToArray();
+        }
     }
-}
