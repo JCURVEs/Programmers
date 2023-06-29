13d915bfa6039ba4000ca1b0266efa5f0a88c940
JCURVEs <97331501+JCURVEs@users.noreply.github.com>
2023-06-29 21:07:49 +0900

    [unrated] Title: 문자열 돌리기, Time: 17.49 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        
+        Console.Clear();
+        
+        for(int i = 0; i < input.Length; i ++)
+        {
+           Console.WriteLine(input[i]);
+        }
+
+        
+    }
+}
